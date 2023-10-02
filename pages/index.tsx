70d2b5193c28df374856c10e5a9f72ad574875dd
Krisztian Bui <buikrisz@gmail.com>
2023-10-02 23:09:58 +0200

    basic css modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { SearchBar, SearchResultsPage } from "@/app/components";
 import styles from "@/app/styles/index.module.css";
+import "../app/styles/globals.css";
 
 const Home = () => {
   return (
