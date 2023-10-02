9ef002d57c636f9ad47216bfe24df3072a3d22aa
Krisztian Bui <buikrisz@gmail.com>
2023-10-02 23:10:11 +0200

    searchbar component created and styled

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,3 +1,31 @@
+import { ChangeEvent, useState, KeyboardEvent } from "react";
+import { BiSearchAlt2 } from "react-icons/bi";
+import styles from "./SearchBar.module.css";
+
 export const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const [searchText, setSearchText] = useState("");
+
+  const onSearchTextChange = (e: ChangeEvent<HTMLInputElement>) => setSearchText(e.target.value);
+  const onEnterClicked = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSearchTriggered();
+    }
+  };
+
+  const onSearchTriggered = () => {
+    if (searchText != null && searchText.length > 0) {
+      console.log(searchText);
+    }
+  };
+
+  return (
+    <div className={styles.searchBar}>
+      <div className={styles.searchBox}>
+        <button className={styles.searchIcon} onClick={onSearchTriggered}>
+          <BiSearchAlt2 />
+        </button>
+        <input className={styles.searchInput} name="searchBox" value={searchText} onChange={onSearchTextChange} onKeyDown={onEnterClicked} />
+      </div>
+    </div>
+  );
 };
